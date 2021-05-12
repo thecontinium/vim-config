ff160dfe6b012d66844024b5f0d9bda793e02688
thecontinium <thecontinium@outlook.com>
2021-05-12 19:31:07 +0100

    turn off p

--- config/local.plugins/which-key.lua
+++ config/local.plugins/which-key.lua
@@ -1,5 +1,29 @@
-require("which-key").setup {
-    -- your configuration comes here
-    -- or leave it empty to use the default settings
-    -- refer to the configuration section below
-  }
+local wk = require("which-key")
+
+wk.setup {
+  plugins = {
+      -- ...
+      presets = {
+        operators = false
+        -- ..
+      },
+    },
+    -- add operators that will trigger motion and text object completion
+    -- to enable all native operators, set the preset / operators plugin above
+    operators = {
+      d = "Delete",
+      c = "Change",
+      y = "Yank (copy)",
+      ["g~"] = "Toggle case",
+      ["gu"] = "Lowercase",
+      ["gU"] = "Uppercase",
+      [">"] = "Indent right",
+      ["<lt>"] = "indent left",
+      ["zf"] = "create fold",
+      ["!"] = "filter though external program",
+      ["v"] = "Visual Character Mode",
+      gc = "Comments"
+  },
+}
+
+wk.register({["p"]="which_key_ignore"})
