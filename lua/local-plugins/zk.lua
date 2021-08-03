117fa7960fc25a3536e96911da8d618c9630f237
thecontinium <thecontinium@outlook.com>
2021-08-03 21:28:33 +0100

    zk telescope

--- lua/local-plugins/zk.lua
+++ lua/local-plugins/zk.lua
@@ -8,20 +8,41 @@ require("zk").setup({
 })
 require("zk").setup_keymaps()
 
-local lspconfig = require('lspconfig')
-local configs = require('lspconfig/configs')
+local function map(mode, lhs, rhs, opts)
+  local map_opts = {noremap = true, silent = true, expr = false}
+  opts = vim.tbl_extend("force", map_opts, opts or {})
+  vim.api.nvim_set_keymap(mode, lhs, rhs, opts)
+end
+-- telescope-zettelkasten
+function _G.search_zettel()
+  require("telescope.builtin").find_files {
+    prompt_title = "Search ZK",
+    hidden = true,
+    shorten_path = false,
+    cwd = vim.env.ZK_NOTEBOOK_DIR or "",
+  }
+end
+require('telescope').load_extension('zk')
+map("n", "<leader>zf", "<cmd>lua _G.search_zettel()<cr>")
+map("n", "<leader>zn", "<cmd>lua require('telescope').extensions.zk.zk_notes()<cr>")
+map("n", "<leader>zg", "<cmd>lua require('telescope').extensions.zk.zk_grep()<cr>")
+map("n", "<leader>zb", "<cmd>lua require('telescope').extensions.zk.zk_backlinks()<cr>")
 
-configs.zk = {
-  default_config = {
-    cmd = {'zk', 'lsp'},
-    filetypes = {'markdown'},
-    root_dir = function()
-      return vim.loop.cwd()
-    end,
-    settings = {}
-  };
-}
-
-lspconfig.zk.setup({ on_attach = function(client, buffer) 
-  -- Add keybindings here, see https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
-end })
+-- 
+-- local lspconfig = require('lspconfig')
+-- local configs = require('lspconfig/configs')
+-- 
+-- configs.zk = {
+--   default_config = {
+--     cmd = {'zk', 'lsp'},
+--     filetypes = {'markdown'},
+--     root_dir = function()
+--       return vim.loop.cwd()
+--     end,
+--     settings = {}
+--   };
+-- }
+-- 
+-- lspconfig.zk.setup({ on_attach = function(client, buffer) 
+--   -- Add keybindings here, see https://github.com/neovim/nvim-lspconfig#keybindings-and-completion
+-- end })
