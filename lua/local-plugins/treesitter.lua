19799ba5bb04d6548bd742f7d06649cbeeb07eed
thecontinium <thecontinium@outlook.com>
2021-10-11 18:24:28 +0100

    turn off treesitter for clojure

---
+++ lua/local-plugins/treesitter.lua
@@ -0,0 +1,36 @@
+-- plugin: nvim-treesitter
+-- see: https://github.com/nvim-treesitter/nvim-treesitter
+-- rafi settings
+
+local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+parser_configs.http = {
+	install_info = {
+		url = 'https://github.com/NTBBloodbath/tree-sitter-http',
+		files = { 'src/parser.c' },
+		branch = 'main',
+	},
+}
+
+require('nvim-treesitter.configs').setup({
+	ensure_installed = 'maintained', -- all, maintained, or list of languages
+	ignore_install = { "clojure" }, -- List of parsers to ignore installing
+	highlight = {
+		enable = true,
+	},
+	-- incremental_selection = {
+	-- 	enable = true,
+	-- 	keymaps = {
+	-- 		init_selection = 'gnn',
+	-- 		node_incremental = 'grn',
+	-- 		scope_incremental = 'grc',
+	-- 		node_decremental = 'grm',
+	-- 	},
+	-- },
+	indent = {
+		enable = true,
+	},
+	refactor = {
+		highlight_definitions = { enable = true },
+		highlight_current_scope = { enable = true },
+	},
+})
