f2a26f7e7dea7ed4ec25734c6f6ba3a1b9de2987
thecontinium <thecontinium@outlook.com>
2021-10-21 17:23:19 +0100

    nerged local plugin configs specific for clojure

--- lua/local-plugins/cmp.lua
+++ lua/local-plugins/cmp.lua
@@ -8,16 +8,16 @@ _G.cmp_source_list = function(arr)
 	local config = {
 		buffer = {
 			name = 'buffer',
-			opts = {
-				-- Use all visible buffers
-				get_bufnrs = function()
-					local bufs = {}
-					for _, win in ipairs(vim.api.nvim_list_wins()) do
-						bufs[vim.api.nvim_win_get_buf(win)] = true
-					end
-					return vim.tbl_keys(bufs)
-				end,
-			},
+			-- opts = {
+			-- 	-- Use all visible buffers
+			-- 	get_bufnrs = function()
+			-- 		local bufs = {}
+			-- 		for _, win in ipairs(vim.api.nvim_list_wins()) do
+			-- 			bufs[vim.api.nvim_win_get_buf(win)] = true
+			-- 		end
+			-- 		return vim.tbl_keys(bufs)
+			-- 	end,
+			-- },
 		},
 		nvim_lsp = { name = 'nvim_lsp' },
 		nvim_lua = { name = 'nvim_lua' },
@@ -65,9 +65,12 @@ vim.api.nvim_exec([[
 	augroup END
 ]], false)
 
-local check_back_space = function()
-	local col = vim.fn.col('.') - 1
-	return col == 0 or vim.fn.getline('.'):sub(col, col):match('%s')
+local has_words_before = function()
+	if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+		return false
+	end
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
 end
 
 local t = function(str)
@@ -92,30 +95,34 @@ cmp.setup {
 		['<C-d>'] = cmp.mapping.scroll_docs(4),
 		['<C-Space>'] = cmp.mapping.complete(),
 		['<C-e>'] = cmp.mapping.close(),
+		['<C-c>'] = function(fallback)
+			cmp.close()
+			fallback()
+		end,
 		['<CR>'] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
 		}),
-		['<Tab>'] = cmp.mapping(function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(t('<C-n>'), 'n')
+		['<Tab>'] = cmp.mapping(function(_)
+			if cmp.visible() then
+				cmp.select_next_item()
 			elseif vim.fn['vsnip#available']() == 1 then
-				vim.fn.feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '')
-			elseif check_back_space() then
-				vim.fn.feedkeys(t('<Tab>'), 'n')
+				vim.api.nvim_feedkeys(t('<Plug>(vsnip-expand-or-jump)'), '', true)
+			elseif has_words_before() then
+				cmp.complete()
 			else
-				fallback()
+				vim.fn.feedkeys(t('<Tab>'), 'n')
 			end
 		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
-			if vim.fn.pumvisible() == 1 then
-				vim.fn.feedkeys(t('<C-p>'), 'n')
+		['<S-Tab>'] = cmp.mapping(function(_)
+			if cmp.visible() then
+				cmp.select_prev_item()
 			elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-				vim.fn.feedkeys(t('<Plug>(vsnip-jump-prev)'), '')
-			elseif check_back_space() then
-				vim.fn.feedkeys(t('<C-h>'), 'n')
+				vim.api.nvim_feedkeys(t('<Plug>(vsnip-jump-prev)'), '', true)
+			elseif has_words_before() then
+				cmp.complete()
 			else
-				fallback()
+				vim.api.nvim_feedkeys(t('<C-h>'), 'n')
 			end
 		end, { 'i', 's' }),
 	},
@@ -162,13 +169,5 @@ cmp.setup {
 		end,
 	},
 
-	event = {
-		on_confirm_done = function(entry)
-			local kind = cmp.lsp.CompletionItemKind
-			local item = entry:get_completion_item()
-			if item.kind == kind.Method or item.kind == kind.Function then
-				vim.api.nvim_feedkeys('(', 'i', true)
-			end
-		end,
-	},
 }
+
