fe4cb167c7df851ad5e784cffde7498db55ed429
thecontinium <thecontinium@outlook.com>
2022-05-04 22:39:19 +0100

    update local plugins to use latest settigs

--- lua/local-plugins/treesitter.lua
+++ lua/local-plugins/treesitter.lua
@@ -2,7 +2,9 @@
 -- see: https://github.com/nvim-treesitter/nvim-treesitter
 -- rafi settings
 
+-- Setup extra parsers.
 local parser_configs = require('nvim-treesitter.parsers').get_parser_configs()
+
 parser_configs.http = {
 	install_info = {
 		url = 'https://github.com/NTBBloodbath/tree-sitter-http',
@@ -12,12 +14,38 @@ parser_configs.http = {
 	filetype = 'http',
 }
 
+-- See https://github.com/nvim-orgmode/orgmode#setup
+-- parser_configs.org = {
+-- 	install_info = {
+-- 		url = 'https://github.com/milisims/tree-sitter-org',
+-- 		revision = 'main',
+-- 		files = {'src/parser.c', 'src/scanner.cc'},
+-- 	},
+-- 	filetype = 'org',
+-- }
+
+-- Setup treesitter
 require('nvim-treesitter.configs').setup({
-	ensure_installed = 'maintained', -- all, maintained, or list of languages
-	ignore_install = { "clojure" }, -- List of parsers to ignore installing
+	-- all, maintained, or list of languages
+	ensure_installed = {
+		'bash', 'c', 'clojure', 'cmake', 'comment', 'commonlisp',
+		'cpp', 'css', 'dart', 'dockerfile', 'dot', 'fennel', 'fish',
+		'go', 'gomod', 'gowork', 'graphql', 'hack', 'haskell', 'hcl',
+		'help', 'html', 'http', 'java', 'javascript', 'jsdoc', 'json',
+		'json5', 'jsonc', 'julia', 'kotlin', 'latex', 'llvm', 'lua',
+		'make', 'markdown', 'ninja', 'nix', 'norg', 'perl', 'php',
+		'pug', 'python', 'query', 'r', 'regex', 'rst', 'ruby', 'rust',
+		'scala', 'scheme', 'scss', 'svelte', 'todotxt', 'toml', 'tsx',
+		'typescript', 'vala', 'vim', 'vue', 'yaml', 'zig',
+	},
+
 	highlight = {
 		enable = true,
+		disable = { 'vim' },
 	},
+
+	additional_vim_regex_highlighting = false,
+
 	-- incremental_selection = {
 	-- 	enable = true,
 	-- 	keymaps = {
@@ -27,13 +55,16 @@ require('nvim-treesitter.configs').setup({
 	-- 		node_decremental = 'grm',
 	-- 	},
 	-- },
+
 	indent = {
 		enable = true,
 	},
+
 	refactor = {
 		highlight_definitions = { enable = true },
 		highlight_current_scope = { enable = true },
 	},
+
 	-- See: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
 	textobjects = {
 		select = {
@@ -49,11 +80,26 @@ require('nvim-treesitter.configs').setup({
 			},
 		},
 	},
+
 	-- See: https://github.com/JoosepAlviste/nvim-ts-context-commentstring
 	context_commentstring = {
 		enable = true,
-		-- The plugin caw.vim will automatically detect and use this plugin itself
+		-- Let other plugins (kommentary) call 'update_commentstring()' manually.
 		enable_autocmd = false,
 	},
+
+	-- See: https://github.com/windwp/nvim-ts-autotag
+	autotag = {
+		enable = true,
+		filetypes = {
+			'html',
+			'javascript',
+			'javascriptreact',
+			'typescriptreact',
+			'svelte',
+			'vue',
+		}
+	}
+
 })
 
