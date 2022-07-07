b84b99a11f459352a3c9c84193f9fa9ec0c647cb
thecontinium <thecontinium@outlook.com>
2022-07-07 21:32:15 +0100

    use upstream cmp changes

--- lua/local-plugins/cmp.lua
+++ lua/local-plugins/cmp.lua
@@ -2,20 +2,20 @@
 -- see: https://github.com/hrsh7th/nvim-cmp
 -- rafi settings
 
-local cmp = require('cmp')
+local cmp = require("cmp")
 
 -- Source setup. Helper function for cmp source presets.
 _G.cmp_get_sources = function(arr)
 	local config = {
-		buffer = { name = 'buffer' },
-		nvim_lsp = { name = 'nvim_lsp' },
-		nvim_lua = { name = 'nvim_lua' },
-		path  = { name = 'path' },
-		emoji = { name = 'emoji' },
-		vsnip = { name = 'vsnip' },
-		tmux  = { name = 'tmux', option = { all_panes = true }},
-		latex = { name = 'latex_symbols' },
-		conjure = { name = 'conjure' },
+		buffer = { name = "buffer" },
+		nvim_lsp = { name = "nvim_lsp" },
+		nvim_lua = { name = "nvim_lua" },
+		path = { name = "path" },
+		emoji = { name = "emoji" },
+		vsnip = { name = "vsnip" },
+		tmux = { name = "tmux", option = { all_panes = true } },
+		latex = { name = "latex_symbols" },
+		conjure = { name = "conjure" },
 	}
 	local sources = {}
 	for _, name in ipairs(arr) do
@@ -31,114 +31,93 @@ local completion_labels = {
 	nvim_lsp = "[LSP]",
 	nvim_lua = "[Lua]",
 	-- buffer   = "[Buf]",
-	spell    = "[Spell]",
-	path     = "[Path]",
+	spell = "[Spell]",
+	path = "[Path]",
 	-- vsnip    = "[VSnip]",
-	tmux     = "[Tmux]",
-	orgmode  = "[Org]"
+	tmux = "[Tmux]",
 }
-
--- Completion sources according to specific file-types.
-vim.api.nvim_exec([[
-	augroup user_cmp
-		autocmd!
-		autocmd FileType markdown,text
-			\ lua require('cmp').setup.buffer{ sources = cmp_get_sources(
-			\ {'emoji', 'nvim_lsp', 'buffer', 'path', 'vsnip', 'tmux'})}
-		autocmd FileType lua
-			\ lua require('cmp').setup.buffer{ sources = cmp_get_sources(
-			\ {'nvim_lua', 'nvim_lsp', 'buffer', 'path', 'vsnip', 'tmux'})}
-		autocmd FileType org
-			\ lua require('cmp').setup.buffer{ sources = cmp_get_sources(
-			\ {'orgmode', 'emoji', 'nvim_lsp', 'buffer', 'path', 'vsnip', 'tmux'})}
-		autocmd filetype clojure
-			\ lua require('cmp').setup.buffer{ sources = cmp_get_sources(
-		  \ { 'nvim_lsp', 'buffer', 'path', 'vsnip', 'tmux', 'conjure' })}
-	augroup END
-]], false)
 
 -- Detect if words are before cursor position.
 local has_words_before = function()
-	if vim.api.nvim_buf_get_option(0, 'buftype') == 'prompt' then
+	if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
 		return false
 	end
 	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 -- Feed proper terminal codes
 local feedkey = function(key, mode)
-	vim.api.nvim_feedkeys(
-		vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
+	vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), mode, true)
 end
 
+-- For LSP integration, see lspconfig.lua
+
 -- :h cmp
-cmp.setup {
+cmp.setup({
 
 	-- Set default cmp sources
 	sources = cmp_get_sources({
-		'nvim_lsp',
-		'buffer',
-		'path',
-		'vsnip',
-		'tmux',
+		"nvim_lsp",
+		"buffer",
+		"path",
+		"vsnip",
+		"tmux",
 	}),
 
 	snippet = {
 		expand = function(args)
 			-- Using https://github.com/hrsh7th/vim-vsnip for snippets.
-			vim.fn['vsnip#anonymous'](args.body)
+			vim.fn["vsnip#anonymous"](args.body)
 		end,
 	},
 
-	mapping = {
-		['<C-Space>'] = cmp.mapping.complete(),
-		['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-		['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-		-- ['<C-y>'] = cmp.config.disable,
-		['<C-e>'] = cmp.mapping({
-			i = cmp.mapping.abort(),
-			c = cmp.mapping.close(),
-		}),
-		['<C-b>'] = cmp.mapping.scroll_docs(-4),
-		['<C-f>'] = cmp.mapping.scroll_docs(4),
-		['<C-u>'] = cmp.mapping(cmp.mapping.select_prev_item(), { 'i', 'c' }),
-		['<C-d>'] = cmp.mapping(cmp.mapping.select_next_item(), { 'i', 'c' }),
-		['<C-c>'] = function(fallback)
+	mapping = cmp.mapping.preset.insert({
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<CR>"] = cmp.mapping.confirm({ select = false }),
+		["<C-p>"] = cmp.mapping.select_prev_item(),
+		["<C-n>"] = cmp.mapping.select_next_item(),
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-u>"] = cmp.mapping(cmp.mapping.select_prev_item(), { "i", "c" }),
+		["<C-d>"] = cmp.mapping(cmp.mapping.select_next_item(), { "i", "c" }),
+		["<C-c>"] = function(fallback)
 			cmp.close()
 			fallback()
 		end,
-		['<CR>'] = cmp.mapping.confirm({
-			behavior = cmp.ConfirmBehavior.Replace,
-			select = false,
-		}),
-		['<Tab>'] = cmp.mapping(function(fallback)
+		["<Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_next_item()
-			elseif vim.fn['vsnip#available']() == 1 then
-				feedkey('<Plug>(vsnip-expand-or-jump)', '')
+			elseif vim.fn["vsnip#available"]() == 1 then
+				feedkey("<Plug>(vsnip-expand-or-jump)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
 				fallback()
 			end
-		end, { 'i', 's' }),
-		['<S-Tab>'] = cmp.mapping(function(fallback)
+		end, { "i", "s" }),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
 			if cmp.visible() then
 				cmp.select_prev_item()
-			elseif vim.fn['vsnip#jumpable'](-1) == 1 then
-				feedkey('<Plug>(vsnip-jump-prev)', '')
+			elseif vim.fn["vsnip#jumpable"](-1) == 1 then
+				feedkey("<Plug>(vsnip-jump-prev)", "")
 			elseif has_words_before() then
 				cmp.complete()
 			else
 				fallback()
 			end
-		end, { 'i', 's' }),
-	},
+		end, { "i", "s" }),
+	}),
 
 	window = {
-		-- completion = cmp.config.window.bordered(),
-		-- documentation = cmp.config.window.bordered(),
+		completion = cmp.config.window.bordered({
+			border = "none",
+			winhighlight = "Normal:Pmenu,FloatBorder:Pmenu,CursorLine:PmenuSel,Search:None",
+		}),
+		documentation = cmp.config.window.bordered({
+			border = "none",
+			winhighlight = "FloatBorder:NormalFloat",
+		}),
 	},
 
 	-- window = {
@@ -167,10 +146,9 @@ cmp.setup {
 		format = function(entry, vim_item)
 			-- Prepend with a fancy icon
 			-- See lua/lsp_kind.lua
-			local symbol = require('lsp_kind').preset()[vim_item.kind]
+			local symbol = require("lsp_kind").preset()[vim_item.kind]
 			if symbol ~= nil then
-				vim_item.kind = symbol
-					.. (vim.g.global_symbol_padding or ' ') .. vim_item.kind
+				vim_item.kind = symbol .. (vim.g.global_symbol_padding or " ") .. vim_item.kind
 			end
 
 			-- Set menu source name
@@ -186,5 +164,17 @@ cmp.setup {
 			return vim_item
 		end,
 	},
-}
+})
 
+-- Completion sources according to specific file-types.
+cmp.setup.filetype({ "markdown", "help", "text" }, {
+	sources = cmp_get_sources({ "emoji", "nvim_lsp", "buffer", "path", "vsnip", "tmux" }),
+})
+
+cmp.setup.filetype({ "lua" }, {
+	sources = cmp_get_sources({ "nvim_lua", "nvim_lsp", "buffer", "path", "vsnip", "tmux" }),
+})
+
+cmp.setup.filetype({ "clojure" }, {
+	sources = cmp_get_sources({ "nvim_lsp", "buffer", "path", "vsnip", "tmux", "conjure" }),
+})
