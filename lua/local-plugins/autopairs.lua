2c6a726a38b95c353c1f4db623cbce80355c9df0
thecontinium <thecontinium@outlook.com>
2023-01-05 13:04:04 +0000

    add commented out autopairs for PR

--- lua/local-plugins/autopairs.lua
+++ lua/local-plugins/autopairs.lua
@@ -20,3 +20,20 @@ npairs.add_rules({
 npairs.get_rule("(")[1].not_filetypes = {"clojure", "scheme", "lisp" }
 
 
+-- local npairs = require("nvim-autopairs")
+-- local cond = require("nvim-autopairs.conds")
+-- local basic = require("nvim-autopairs.rules.basic")
+
+-- npairs.setup()
+
+-- -- this in effect uses the default behaviour and adds +,-,/,* to no after
+-- local bracket = basic.bracket_creator(require("nvim-autopairs").config)
+-- npairs.add_rules({
+--	bracket("(", ")", { "clojure", "lisp" })
+--		:with_pair(cond.not_after_regex([=[[%w%%%'%[%"%.%`%$%+%-%/%*]]=])),
+-- })
+
+-- -- turn off the orighinal rule for clojure and lisp
+-- npairs.get_rule("(")[1].not_filetypes = { "clojure", "lisp" }
+
+
