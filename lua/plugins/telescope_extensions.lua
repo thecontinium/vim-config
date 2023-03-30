cbbd4e1e89ab3bbc3f52b0895ac137c6bb8637d9
thecontinium <thecontinium@outlook.com>
2023-03-30 09:15:53 +0100

    add telescope_extensions

---
+++ lua/plugins/telescope_extensions.lua
@@ -0,0 +1,31 @@
+return {
+	{
+		"debugloop/telescope-undo.nvim",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<localLeader>y", "<cmd>Telescope undo<cr>", desc = "Telescope undo", },
+		},
+		config = function()
+			require("telescope").load_extension("undo")
+			-- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
+		end,
+	},
+
+	{
+		"LukasPietzschmann/telescope-tabs",
+		dependencies = {
+			"nvim-telescope/telescope.nvim",
+		},
+		keys = {
+			{ "<localLeader><tab>", "<cmd>Telescope telescope-tabs list_tabs<cr>", desc = "Telescope tabs", },
+		},
+		config = function()
+			require("telescope-tabs").setup({
+				close_tab_shortcut_i = "<C-d>", -- if you're in insert mode
+				close_tab_shortcut_n = "dd", -- if you're in normal mode
+			})
+		end,
+	},
+}
