838ccf207d9e1efe24e2fbbf8d0b4f1f0da2ffd0
thecontinium <thecontinium@outlook.com>
2023-04-07 17:52:55 +0100

    move to lazy

--- lua/local-plugins/leap.lua
+++
@@ -1,8 +0,0 @@
-
-vim.keymap.set({'n', 'x', 'o'}, 'f', '<Plug>(leap-forward-to)')
-vim.keymap.set({'n', 'x', 'o'}, 't', '<Plug>(leap-forward-till)')
-vim.keymap.set({'n', 'x', 'o'}, 'F', '<Plug>(leap-backward-to)')
-vim.keymap.set({'n', 'x', 'o'}, 'T', '<Plug>(leap-backward-till)')
-
-
-
