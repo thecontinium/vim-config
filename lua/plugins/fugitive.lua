74f5f452f46f59c298ae260dd352027deb65507c
thecontinium <thecontinium@outlook.com>
2023-04-17 15:00:20 +0100

    simplify additional keys

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -1,25 +1,8 @@
 return {
 	{
 		'tpope/vim-fugitive',
-		dependencies = { 'tpope/vim-rhubarb' },
-		cmd = { 'G', 'Git', 'GBrowse', 'Gfetch', 'Gclog', 'Gdiffsplit' },
 		keys = {
-			{ '<leader>gd', '<cmd>Gdiffsplit<CR>' },
-			{ '<leader>gb', '<cmd>Git blame<CR>' },
-			{ '<leader>go', '<cmd>GBrowse<CR>' },
-			{ '<leader>go', ':GBrowse<CR>', mode = 'x' },
-			{ '<leader>gp', '<cmd>G push<CR>'},
+			{ '<leader>gp', '<cmd>G push<CR>' , desc = "Git push"},
 		},
-		config = function()
-			vim.api.nvim_create_autocmd('FileType', {
-				group = vim.api.nvim_create_augroup('rafi_fugitive', {}),
-				pattern = 'fugitiveblame',
-				callback = function()
-					vim.schedule(function()
-						vim.cmd.normal('A')
-					end)
-				end
-			})
-		end
 	},
 }
--- lua/plugins/zk.lua
+++ lua/plugins/zk.lua
@@ -1,19 +1,8 @@
 return {
 	{
 		"mickael-menu/zk-nvim",
-		name = "zk",
-		ft = "markdown",
-		cmd = { "ZkNew", "ZkNotes", "ZkTags", "ZkMatch" },
 		keys = {
-			{ "<leader>zn", "<Cmd>ZkNew { title = vim.fn.input('Title: ') }<CR>", desc = "Zk New" },
-			{ "<leader>zo", "<Cmd>ZkNotes { sort = { 'modified' } }<CR>", desc = "Zk Notes" },
-			{ "<leader>zt", "<Cmd>ZkTags<CR>", desc = "Zk Tags" },
-			{ "<leader>zf", "<Cmd>ZkNotes { sort = { 'modified' }, match = vim.fn.input('Search: ') }<CR>", desc = "Zk Search", },
-			{ "<leader>zf", ":'<,'>ZkMatch<CR>", mode = "x", desc = "Zk Match" },
-			{ "<leader>zb", "<Cmd>ZkBacklinks<CR>", desc = "Zk Backlinks" },
-			{ "<leader>zl", "<Cmd>ZkLinks<CR>", desc = "Zk Links" },
 			{ "<leader>zd", "<Cmd>ZkNew {  dir=vim.env.ZK_NOTEBOOK_DIR .. '/journal/daily' }<CR>", desc = "Zk Diary" },
 		},
-		opts = { picker = "telescope" },
 	},
 }
