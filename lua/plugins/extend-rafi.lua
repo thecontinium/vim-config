a0115e5fa206c28a3fa9b4c6b60bd59f383646a2
thecontinium <thecontinium@outlook.com>
2024-02-14 14:14:36 +0000

    remove mason tool installer

--- lua/plugins/extend-rafi.lua
+++ lua/plugins/extend-rafi.lua
@@ -23,3 +23,25 @@ return {
 		},
 	}
 }
+	-- {
+	-- 	'mfussenegger/nvim-lint',
+	-- 	opts = {
+	-- 		linters_by_ft = {
+	-- 			python = { 'mypy', 'ruff' },
+	-- 			-- Use the "*" filetype to run linters on all filetypes.
+	-- 			-- ['*'] = { 'global linter' },
+	-- 			-- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+	-- 			-- ['_'] = { 'fallback linter' },
+	-- 		},
+	-- 	},
+	-- },
+	--
+	-- {
+	-- 	'stevearc/conform.nvim',
+	-- 	opts = {
+	-- 		formatters_by_ft = {
+	-- 			python = { "black" },
+	-- 		},
+	-- 	},
+	-- },
+	--
