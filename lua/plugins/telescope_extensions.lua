16b980b38edc84bc0593c1d246570c6f8e778bbc
thecontinium <thecontinium@outlook.com>
2024-10-30 15:31:49 +0000

    feat: add telescope undo and tabs

--- lua/plugins/telescope_extensions.lua
+++ lua/plugins/telescope_extensions.lua
@@ -1,30 +1,34 @@
 return {
 	{
-		"debugloop/telescope-undo.nvim",
+		'debugloop/telescope-undo.nvim',
 		dependencies = {
-			"nvim-telescope/telescope.nvim",
+			'nvim-telescope/telescope.nvim',
 		},
 		keys = {
-			{ "<localLeader>p", "<cmd>Telescope undo<cr>", desc = "Telescope undo", },
+			{ '<localLeader>p', '<cmd>Telescope undo<cr>', desc = 'Telescope undo' },
 		},
 		config = function()
-			require("telescope").load_extension("undo")
+			require('telescope').load_extension('undo')
 			-- optional: vim.keymap.set("n", "<leader>u", "<cmd>Telescope undo<cr>")
 		end,
 	},
 
 	{
-		"LukasPietzschmann/telescope-tabs",
+		'LukasPietzschmann/telescope-tabs',
 		dependencies = {
-			"nvim-telescope/telescope.nvim",
+			'nvim-telescope/telescope.nvim',
 		},
 		keys = {
-			{ "<localLeader><tab>", "<cmd>Telescope telescope-tabs list_tabs<cr>", desc = "Telescope tabs", },
+			{
+				'<localLeader><tab>',
+				'<cmd>Telescope telescope-tabs list_tabs<cr>',
+				desc = 'Telescope tabs',
+			},
 		},
 		config = function()
-			require("telescope-tabs").setup({
-				close_tab_shortcut_i = "<C-d>", -- if you're in insert mode
-				close_tab_shortcut_n = "dd", -- if you're in normal mode
+			require('telescope-tabs').setup({
+				close_tab_shortcut_i = '<c-d>', -- if you're in insert mode
+				close_tab_shortcut_n = 'dd', -- if you're in normal mode
 			})
 		end,
 	},
