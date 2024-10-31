c083adf3d00d603affbe6d619789303dd3cdd760
thecontinium <thecontinium@outlook.com>
2024-10-31 18:54:19 +0000

    feat:add codeium

--- lua/plugins/extras.lua
+++ lua/plugins/extras.lua
@@ -3,9 +3,9 @@ return {
 	{ import = 'lazyvim.plugins.extras.coding.yanky' },
 	{ import = 'rafi.plugins.extras.coding.align' },
 	{ import = 'rafi.plugins.extras.editor.harpoon2' },
-
 	{ import = 'rafi.plugins.extras.git.cmp-git' },
 
+	{ import = 'lazyvim.plugins.extras.coding.codeium' },
 	{ import = 'lazyvim.plugins.extras.lang.docker' },
 	{ import = 'rafi.plugins.extras.lang.tmux' },
 
