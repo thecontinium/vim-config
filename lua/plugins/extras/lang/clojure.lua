a5ebf2067e5698f8b9112972fe3e68f076a5060a
thecontinium <thecontinium@outlook.com>
2025-09-29 17:44:20 +1000

    add clojure

--- lua/plugins/extras/lang/clojure.lua
+++ lua/plugins/extras/lang/clojure.lua
@@ -1,6 +1,35 @@
 return {
 
   { import = "lazyvim.plugins.extras.lang.clojure" },
+  { import = "lazyvim.plugins.extras.coding.mini-surround" },
+  {
+    "Olical/conjure",
+    dependencies = {
+      "folke/which-key.nvim",
+    },
+    init = function()
+      vim.api.nvim_create_autocmd("filetype", {
+        group = vim.api.nvim_create_augroup("group_conjure-wk", {}),
+        pattern = vim.g["conjure#filetypes"],
+        callback = function(args)
+          local wk = require("which-key")
+          wk.add({
+            -- add conjure group names
+            { "<localleader>c", mode = "n", buffer = args.buf, group = "+connect" },
+            { "<localleader>e", mode = "n", buffer = args.buf, group = "+evaluate" },
+            { "<localleader>ec", mode = "n", buffer = args.buf, group = "+comment" },
+            { "<localleader>g", mode = "n", buffer = args.buf, group = "+get" },
+            { "<localleader>l", mode = "n", buffer = args.buf, group = "+log" },
+            { "<localleader>r", mode = "n", buffer = args.buf, group = "+refresh" },
+            { "<localleader>s", mode = "n", buffer = args.buf, group = "+session" },
+            { "<localleader>t", mode = "n", buffer = args.buf, group = "+test" },
+            { "<localleader>v", mode = "n", buffer = args.buf, group = "+display" },
+            { "<localleader>x", mode = "n", buffer = args.buf, group = "+run" },
+          })
+        end,
+      })
+    end,
+  },
   {
     "julienvincent/nvim-paredit",
     config = function()
@@ -17,6 +46,38 @@ return {
             mode = { "n", "x", "v" },
           },
         },
+      })
+    end,
+    init = function()
+      vim.api.nvim_create_autocmd("filetype", {
+        group = vim.api.nvim_create_augroup("group_treesitter-sexp", {}),
+        pattern = "clojure",
+        callback = function()
+          vim.keymap.set(
+            "n",
+            "<localleader>w",
+            "gsaie)(a <Left>",
+            { buffer = true, remap = true, desc = "insert surround elem head" }
+          )
+          vim.keymap.set(
+            "n",
+            "<localleader>W",
+            "gsaie))i ",
+            { buffer = true, remap = true, desc = "insert surround elem tail" }
+          )
+          vim.keymap.set(
+            "n",
+            "<localleader>i",
+            "gsaif)((a <Left>",
+            { buffer = true, remap = true, desc = "insert surround form head" }
+          )
+          vim.keymap.set(
+            "n",
+            "<localleader>I",
+            "gsaif)))i ",
+            { buffer = true, remap = true, desc = "insert surround form tail" }
+          )
+        end,
       })
     end,
   },
