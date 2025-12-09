dfce90514d79dee43afab25ce0403c2dfd925e6b
thecontinium <thecontinium@outlook.com>
2025-12-09 16:21:36 +1100

    add default config for markdown lint/conform markdownlint

--- lua/plugins/extras/lang/markdown.lua
+++ lua/plugins/extras/lang/markdown.lua
@@ -1,6 +1,32 @@
 return {
   { import = "lazyvim.plugins.extras.lang.markdown" },
   { "iamcco/markdown-preview.nvim", enabled = false }, -- No Default Markdown Preview
+
+  {
+    "mfussenegger/nvim-lint",
+    opts = {
+      linters = {
+        ["markdownlint-cli2"] = {
+          args = { "--config", vim.fn.expand("$XDG_CONFIG_HOME/markdownlint/.markdownlint-cli2.yaml"), "--" },
+        },
+      },
+    },
+  },
+  {
+    "stevearc/conform.nvim",
+    opts = {
+      formatters = {
+        ["markdownlint-cli2"] = {
+          args = {
+            "--config",
+            vim.fn.expand("$XDG_CONFIG_HOME/markdownlint/.markdownlint-cli2.yaml"),
+            "--fix",
+            "$FILENAME",
+          },
+        },
+      },
+    },
+  },
   {
     "thecontinium/bullets.nvim",
     ft = "markdown",
